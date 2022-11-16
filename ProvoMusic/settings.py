85f5213b9ff40b11dd67b3de293b6948bca0bf4b
5kiing <masonj.frodsham@gmail.com>
2022-11-16 16:48:12 -0700

    postgreSQL commit

--- ProvoMusic/settings.py
+++ ProvoMusic/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'ProvoMusic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'event',
+    'USER': 'postgres',
+    'PASSWORD': 'music',
+    'HOST': 'localhost'
     }
 }
 
@@ -127,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    event_name = models.CharField(max_length=30)
+    group_name = models.CharField(max_length=40)
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    event_location = models.CharField(max_length=50)
+
+    def __str__(self) :
+        return(self.event_name)
+
+    class Meta :
+        db_table = 'Event'
+
